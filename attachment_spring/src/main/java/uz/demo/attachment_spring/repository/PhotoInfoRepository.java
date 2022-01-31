f9e78bf256b007bd47e7f67b098f0bf8b19cbfb6
Mangust02 <accformy2@gmail.com>
2022-01-31 15:46:02 +0500

    added

--- attachment_spring/src/main/java/uz/demo/attachment_spring/repository/PhotoInfoRepository.java
+++ attachment_spring/src/main/java/uz/demo/attachment_spring/repository/PhotoInfoRepository.java
@@ -1,4 +1,10 @@
 package uz.demo.attachment_spring.repository;
 
-public interface PhotoInfoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import uz.demo.attachment_spring.entity.PhotoInfo;
+
+
+@Repository
+public interface PhotoInfoRepository extends JpaRepository<PhotoInfo, Long> {
 }
