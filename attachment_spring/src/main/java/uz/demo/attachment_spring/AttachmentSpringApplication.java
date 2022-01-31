5665451fef7a4ec1706e4d1cbfff30e3cc809018
Mangust02 <accformy2@gmail.com>
2022-01-31 15:23:24 +0500

    module added

---
+++ attachment_spring/src/main/java/uz/demo/attachment_spring/AttachmentSpringApplication.java
@@ -0,0 +1,13 @@
+package uz.demo.attachment_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttachmentSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttachmentSpringApplication.class, args);
+    }
+
+}
---
+++ attachment_spring/src/test/java/uz/demo/attachment_spring/AttachmentSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.demo.attachment_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttachmentSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
